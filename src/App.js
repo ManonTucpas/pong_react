26c2b4a4df97ec41d28cf894f9ff0e2efb787886
Manon LECUYER <mlecuyer@e3r4p14.clusters.42paris.fr>
2022-05-19 18:20:01 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Box from './components/Box';
+import { BACKGROUND,  BALL } from './components/Box';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+/* taille du tableau de jeu */
+const ROW_SIZE = 10;
+const COL_SIZE = 20;
+
+/* taille  barre de joueur  
+		 _
+		|_|		
+		|_|
+		|_|
+*/
+const PADDLE_BOARD_SIZE = 3;
+const PADDLE_EDGE_SPACE = 1; // espacee de 1 du bord
+
+/* input utilisateur */
+// const PLAYER_UP   = 38; // up arrow
+// const PLAYER_DOWN = 40;  // down arrow
+// const PAUSE       = 32;  // space
+
+/* affichage de tableau 2d
+	fr est une unite, chaque case fera 1 fr */
+const style = {
+	widht: "250px",
+	height: "250px",
+	display: "grid",
+	gridTemplate: `repeat(${ROW_SIZE}, 1fr / repeat(${COL_SIZE}, 1fr)`
+}
+
+const inner = {
+	display: "flex",
+	flexDirection: "row",
+	justifyContent: "justify",
+	marginTop: "9em",
+	marginLeft: "25em",
+	Text: "100px",
+	padding: "10px"
+}
+
+const InitialState = () => {
+		
+	const paddle = [...Array(PADDLE_BOARD_SIZE)].map((_, pos) => pos);
+	return {
+		/* on cree la barre de jeu
+			On a definitle Array(3) => board_size
+			et on itere 3x fois pour creer la barre, a partir de la position 
+			si x = 0 la barre sera sur le 1,
+			et va s'etendre sur trois case x++ sur le 21 et x++ sur le 31*/
+		player: paddle.map(x => (x * COL_SIZE) + PADDLE_EDGE_SPACE), // player sera a gauche du tab
+		oponnent: paddle.map(x => (x * COL_SIZE) - (PADDLE_EDGE_SPACE + 1)), // opponent sera a droite du tab
+		ball: Math.round((ROW_SIZE * COL_SIZE) / 2) + ROW_SIZE,  // on positionne la balle au milieu
+		ballSpeed: 100,
+		deltaY: -COL_SIZE,
+		deltaX: -1, // si -1 la balle va vers le player / si 1 elle va vers l'opposant
+		pause: true, // pour commencer le jeu
+		// /* for dumb AI? */
+		// opponentSpeed: 150,
+		// opponentDir: false,
+		/* Score */
+		playerScore: 0,
+		opponentScore: 0, 
+	}
+}
+
+class App extends Component {
+
+	
+
+	constructor(props) {
+		super(props);
+		this.state = InitialState();
+	}
+
+
+	/*	reset the game 
+			quand le jeu est reste on remt la balle au milieu
+	*/
+	resetGame = () => this.setState({
+		ball: Math.round((ROW_SIZE * COL_SIZE) / 2) + ROW_SIZE,
+	})
+
+	/* check si on peut bouger le planche du palyer ou de celle de l'opposant */
+	moveBoard = (playerBoard, isUp) => {}
+
+	/* check si la balle touche les bords du tableau */
+	touchingEdge = (pos) => {}
+
+	/* check si la balle touche la barre du joueur -en vertical- */
+	touchingPaddle = (pos)=> {}
+
+	/* check si la ball touche le haut ou bas dela barre du jouer -horizontal- */
+	touchingPaddleEdge = (pos) => {}
+
+	/* check si la balle a marque un point */
+	isScore = (pos) => {}
+
+	/* fait rebondir la balle */
+	bounceBall = () => {}
+
+	/* gere les touches pressees par l'utilisateur */
+	keyInput = ({keyCode}) => {}
+
+	/* le rendu de tout le code 'mis' sur la page */
+
+
+
+	render() {
+
+		//on itere case par case sur le tableau
+		const board = [...Array(ROW_SIZE * COL_SIZE)].map((_, pos) => {
+			let val = BACKGROUND;
+			// indoexOf() renvoie le premier indice pour lequel on trouve un element donne dans le tableau
+			// si rien n'est trouve on renvoit -1 donc dans la condition ci dessus , si !== -1 on a trouve la position 
+			// if ((this.state.player.indexOf(pos) !== -1) || (this.state.opponent.indexOf(pos) !== -1)) {
+			// 	val = PLAYER;
+			// }
+			if (this.state.ball === pos) {
+				val = BALL;
+			}
+			console.log('cic')
+			return <Box key= {pos} name={val} />;
+		})
+		return (
+			<div style={inner}>
+				{/* on retourne le retour de board a chaque position */}
+					<div style={style}>{board}</div> 
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Box.jsx
@@ -0,0 +1,66 @@
+import React from "react"
+
+
+/* style tableau */ 
+const backgroundStyle = {
+    height: "35px",
+    width: "35px",
+    borderStyle: "solid",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: "2px"
+}
+
+const playerStyle = {
+    height: "35px",
+    width: "35px",
+    borderStyle: "solid",
+    justifyContent: "center",
+    backgroundColor: "blue",
+    color: "white"
+}
+
+const ballStyle = {
+    height: "35px",
+    width: "35px",
+    display: "block",
+    backgroundColor: "yellow",
+    justifyContent: "center",
+    borderRadius: "100%",
+    color:"white"
+}
+
+/* enum */
+const BACKGROUND = 0;
+const PLAYER = 1;
+const BALL = 2;
+export {
+    BACKGROUND,
+    PLAYER,
+    BALL,
+}
+
+
+/* fonction pour savoir quel style on veut */
+const getStyle = (val) => {
+    if (val === BACKGROUND) {
+        return {};
+    }
+    if (val === PLAYER) {
+        return playerStyle;
+    }
+    else {
+        return ballStyle;
+    }
+}
+
+
+/* La box sera utilisee pour representer le background, les joueurs et la ball
+    en fonction de la position donne par le tableau 
+    getStyle permet de recuperer si on veut le stylePlayer ou styleBall*/
+
+const Box = (props) => <div style = {backgroundStyle}>
+                     <div style={getStyle(props.name)} />    
+</div>
+
+export default Box
